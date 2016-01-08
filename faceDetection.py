bcf115fa2bb7745b473a983a924f4c8805e510c0
Sarath M <sarathm00@gmail.com>
2016-01-08 12:29:11 +0530

    Face detection using Haar Cascades

---
+++ faceDetection.py
@@ -0,0 +1,39 @@
+import cv2
+
+#Load a cascade file for detecting faces
+faceCascade = cv2.CascadeClassifier('./faces.xml')	# Provide correct path to faces.xml
+
+#to use video file instead of camera
+cap = cv2.VideoCapture('openCV2.mp4')
+
+#To use live feed from camera
+#cap = cv2.VideoCapture(0)
+
+while True:
+
+	# read frames from captured video
+	ret,frame = cap.read()
+
+	#Convert to grayscale
+	gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+	#Look for faces in the image using the loaded cascade file
+	faces = faceCascade.detectMultiScale(gray,1.1,5)
+
+	#Draw a rectangle around every found face
+	for (x,y,w,h) in faces:
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+
+	# show resulting video(played frame by frame) with rectangles around detected face	
+	cv2.imshow('frame',frame)
+	
+	#use waitKey(1) for live feed from camera
+	# Press 'q' to stop program execution
+	if cv2.waitKey(20) & 0xff == ord('q'):
+		break
+
+#Stop capture
+cap.release()
+
+# Properly close all windows
+cv2.destroyAllWindows()
