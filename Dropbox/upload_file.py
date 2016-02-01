ef16d6f3046c52b18aaf6e3fbfe0a043adcac2ca
Toshiba Laptop <sarathm00@gmail.com>
2016-02-01 21:16:14 +0530

    Working

--- Dropbox/upload_file.py
+++ Dropbox/upload_file.py
@@ -1,9 +1,17 @@
 import dropbox
+import os,re
 
 access_token = 'gMLhtGxtfGAAAAAAAAAACDBrTIGSl3zNtH_TiZVuIl-IdchTWT4HCPXo_Z1s6D3q'
 client = dropbox.client.DropboxClient(access_token)
 #print 'linked account: ', client.account_info()
 
-f = open('../Detected10.jpg', 'rb')
-response = client.put_file('/Detected10.jpg', f)
-print "uploaded:", response
+while True:
+    try:
+        for f in os.listdir('../Detected/'):
+            if re.search('.*\.jpg', f):
+                fl = open('../Detected/'+f, 'rb')
+                response = client.put_file('/'+ f, fl)
+                print "uploaded file", response['path'].split('/')[1]
+                os.remove('../Detected/'+f)
+    except Exception, e:
+        print e
